e63ccff7fb87388f60fe41aeca226fe97d1ab0bc
Oliver Sokol <oliver.sokol@atea.no>
2023-01-02 14:25:09 +0100

    1.1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,25 +1,52 @@
+const Header = (props) => {
 
-const Hello = (props) => {
-  return(
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
+  return (
+    <h1>{props.course}</h1>
+  );
+}
+
+const Content = (props) => {
+
+  return (
+    <>
+    <p>
+        {props.part1} {props.exercieses1}
       </p>
-    </div>
-  )
+      <p>
+        {props.part2} {props.exercieses2}
+      </p>
+      <p>
+        {props.part3} {props.exercieses3}
+      </p>
+    </>
+  );
+}
+
+const Total = (props) => {
+
+  return (
+    <p>Number of exercises {props.total}</p>
+  );
 }
 
 const App = () => {
 
-  const name = "Peter"
-  const age = 10
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercieses1 = 10
+  const part2 = "Using props to pass data"
+  const exercieses2 = 7
+  const part3 = "State of component"
+  const exercieses3 = 14
 
 
   return(
     <div>
-      <h1>Greetings</h1>
-      <Hello name="Maya" age={26+10}/>
-      <Hello name={name} age={age}/>
+      <Header course = {course} />
+      <Content part1={part1} part2={part2} part3={part3} exercieses1={exercieses1} exercieses2={exercieses2} exercieses3={exercieses3}/>
+      <Total total={exercieses1 + exercieses2 + exercieses3}/>
+
+      
     </div>
     );
 }
